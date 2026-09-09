762188b86df964ccf8e143fc302a07485e49a0d3
Adnan Dwere <adnandwere1@gmail.com>
2026-09-09 16:47:22 +0300

    add cv

--- app/resume/page.js
+++ app/resume/page.js
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Link from "next/link";
 
-const cvHref = "/Adnan-Dwere-CV.pdf";
+const cvHref = "/MY-CV1.pdf";
 
 export default function ResumePage() {
   const [language, setLanguage] = useState("en");
